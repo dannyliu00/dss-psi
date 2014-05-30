1f26b37522b52cd9b9531f127faf63baff079b08
Brady Erickson <brady.erickson@polaris.com>
2014-05-30 11:44:02 -0500

    Initial creation

---
+++ src/main/java/com/polaris/psi/resource/dto/OrderSegmentWithRange.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package com.polaris.psi.resource.dto;
+
+/**
+ * @author bericks
+ *
+ */
+public class OrderSegmentWithRange extends OrderSegmentDTO {
+
+	private static final long serialVersionUID = -7875018740992473600L;
+	
+	private int minRecommend;
+	private int maxRecommend;
+
+	/**
+	 * @return the minRecommend
+	 */
+	public int getMinRecommend() {
+		return minRecommend;
+	}
+
+	/**
+	 * @param minRecommend the minRecommend to set
+	 */
+	public void setMinRecommend(int minRecommend) {
+		this.minRecommend = minRecommend;
+	}
+
+	/**
+	 * @return the maxRecommend
+	 */
+	public int getMaxRecommend() {
+		return maxRecommend;
+	}
+
+	/**
+	 * @param maxRecommend the maxRecommend to set
+	 */
+	public void setMaxRecommend(int maxRecommend) {
+		this.maxRecommend = maxRecommend;
+	}
+	
+}
