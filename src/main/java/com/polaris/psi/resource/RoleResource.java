0633b347a98b24ddf273719424db366bb27627eb
Brady Erickson <brady.erickson@polaris.com>
2014-06-06 17:27:27 -0500

    Initial creation

---
+++ src/main/java/com/polaris/psi/resource/RoleResource.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.polaris.psi.resource;
+
+import groovy.util.logging.Slf4j;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.polaris.pwf.session.SessionHelper;
+import com.polaris.pwf.session.UserData;
+
+/**
+ * @author bericks
+ *
+ */
+@Slf4j
+@Component
+@Path("/roles")
+public class RoleResource {
+	
+	@Autowired
+	SessionHelper sessionHelper;
+
+	@GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=UTF-8")
+	public UserData getRole() {
+		
+		return sessionHelper.getUserData();
+		
+	}
+}
