8c6bbce4e4931e522bc8c81ec9e0099bf9fe0d91
Brady Erickson <brady.erickson@polaris.com>
2014-06-06 15:52:11 -0500

    Initial creation of app routing module

---
+++ src/main/webapp/js/utilities/routing/routing-paths-module.js
@@ -0,0 +1,16 @@
+(function() {
+    angular.module('sellIn.routing.paths', [])
+        .constant('defaultPageUrl', '/default')
+        .constant('defaultPageControllerName', 'defaultPageController')
+        .constant('defaultPageTemplateUrl', 'js/pages/dealer-summary/profile-summary.html')
+
+        .constant('dealerSummaryPageUrl', '/dealerSummary/:dealerId')
+        .constant('dealerSummaryPageControllerName', 'dealerProfileSummaryCtrl')
+        .constant('dealerSummaryPageTemplateUrl', 'js/pages/dealer-summary/profile-summary.html')
+
+        .constant('profilePageUrl', '/dealerProfile/:dealerId/profile/:profileId/type/:type')
+        .constant('profilePageControllerName', 'profileController')
+        .constant('profilePageTemplateUrl', 'js/pages/profile-detail/profile-detail-template.html')
+
+        .constant('defaultRedirection', '/default');
+}());
