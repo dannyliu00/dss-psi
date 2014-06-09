27c63f49c8fb91a1e6b7b15c2799edf803100499
Brady Erickson <brady.erickson@polaris.com>
2014-06-09 17:22:49 -0500

    Added logging if dao List results contains more than one entry.

--- src/main/java/com/polaris/psi/repository/dao/DealerAndDsmDao.java
+++ src/main/java/com/polaris/psi/repository/dao/DealerAndDsmDao.java
@@ -7,8 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Repository;
 
 import com.polaris.psi.repository.entity.DealerAndDsm;
@@ -21,7 +20,7 @@ import com.polaris.pwf.dao.AbstractPolarisDealersExtensionDao;
 @Repository
 public class DealerAndDsmDao extends AbstractPolarisDealersExtensionDao<DealerAndDsm> {
 	
-	private Log log = LogFactory.getLog(this.getClass());
+	private static final Logger log = Logger.getLogger(DealerAndDsmDao.class);
 	
 	public DealerAndDsmDao() {
 		super(DealerAndDsm.class);
@@ -44,7 +43,8 @@ public class DealerAndDsmDao extends AbstractPolarisDealersExtensionDao<DealerAn
         try {
             if(dsms.size() > 1) throw new Exception();
         } catch (Exception e) {
-        	
+        	log.error("We expected to get a List with a single item. The List contained " + dsms.size() + " instead."
+        			+ " Returning the first entry.");
         }
         
         return dsms.get(0);
