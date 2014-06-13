1be3a71231c5a880db864a936b0905d5e0272a0b
tbpitts <terry.pitts@polaris.com>
2014-06-13 16:33:32 -0500

    Adding datatables to profile summary page

--- src/main/webapp/js/pages/dealer-summary/profile-summary-controller.js
+++ src/main/webapp/js/pages/dealer-summary/profile-summary-controller.js
@@ -4,24 +4,29 @@
 (function() {
     var dealerProfileSummary = sellInNamespace('sellIn.pages.dealerProfileSummary');
 
-    function DealerProfileSummaryCtrl ($scope, $routeParams, $location, dealerResource, dealerProfilesResource, profilePageUrl) {
+    function DealerProfileSummaryCtrl ($scope, $routeParams, $location, dealerResource, dealerProfilesResource, profilePageUrl,  DTOptionsBuilder) {
         this.location = $location;
+            
+	    var dealer = {dealerId: $routeParams.dealerId};
+	    dealerResource.get(dealer).then(function(returnedDealer) {
+	    	$scope.dealer = returnedDealer;
+	        });
 
-        var dealer = {dealerId: $routeParams.dealerId};
-        dealerResource.get(dealer).then(function(returnedDealer) {
-            $scope.dealer = returnedDealer;
-        });
-
-        dealerProfilesResource.query(dealer).then(function(returnedProfiles) {
-            $scope.profiles = returnedProfiles;
-        });
-
+	    dealerProfilesResource.query(dealer).then(function(returnedProfiles) {
+	        $scope.profiles = returnedProfiles;
+	        });
+	    
         $scope.navigateToProfile = function(dealerId, profileId, type) {
             var finalUrl = profilePageUrl.replace(':dealerId', dealerId)
                 .replace(':profileId', profileId)
                 .replace(':type', type);
             $location.path(finalUrl);
         };
+        
+        $scope.dtOptions = DTOptionsBuilder.newOptions()
+	        .withPaginationType('full_numbers')
+	        .withDisplayLength(10)
+	        .withBootstrap();
     }
 
     dealerProfileSummary.DealerProfileSummaryCtrl = DealerProfileSummaryCtrl;
--- src/main/webapp/js/pages/dealer-summary/profile-summary-module.js
+++ src/main/webapp/js/pages/dealer-summary/profile-summary-module.js
@@ -1,6 +1,6 @@
 (function() {
     var dealerProfileSummary = sellInNamespace('sellIn.pages.dealerProfileSummary');
 
-    angular.module('sellIn.pages.dealerProfileSummary', ['sellIn.resources.dealer', 'sellIn.resources.dealerProfiles', 'sellIn.routing.paths'])
+    angular.module('sellIn.pages.dealerProfileSummary', ['sellIn.resources.dealer', 'sellIn.resources.dealerProfiles', 'sellIn.routing.paths','datatables'])
         .controller('dealerProfileSummaryCtrl', dealerProfileSummary.DealerProfileSummaryCtrl);
-}());
+})();
