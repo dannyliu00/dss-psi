a81a06036797588c2d3ae5b75888c3ef06911afc
Brady Erickson <brady.erickson@polaris.com>
2014-06-18 12:33:48 -0500

    Initial creation

---
+++ src/main/java/com/polaris/psi/repository/entity/SegmentPK.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.polaris.psi.repository.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author bericks
+ *
+ */
+public class SegmentPK implements Serializable {
+
+	private static final long serialVersionUID = -1358225220493787351L;
+
+	private String name;
+
+	private String type;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
