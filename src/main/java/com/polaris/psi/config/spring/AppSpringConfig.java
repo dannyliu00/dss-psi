879e5f716b82edd3bfea0102d56f32f205f49921
Brady Erickson <brady.erickson@polaris.com>
2014-06-23 08:16:39 -0500

    Removed application caching code

--- src/main/java/com/polaris/psi/config/spring/AppSpringConfig.java
+++ src/main/java/com/polaris/psi/config/spring/AppSpringConfig.java
@@ -2,31 +2,34 @@ package com.polaris.psi.config.spring;
 
 import javax.sql.DataSource;
 
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import com.polaris.psi.Constants;
 import com.polaris.pwf.config.spring.PwfSpringConfig;
 import com.polaris.pwf.config.spring.datasource.PolarisDealersCommonDataSource;
 import com.polaris.pwf.config.spring.datasource.PolarisDealersDataSource;
 import com.polaris.pwf.config.spring.datasource.PolarisDealersExtensionDataSource;
+import com.polaris.pwf.config.spring.datasource.PolarisMinneapolisDataSource;
 import com.polaris.pwf.util.JndiUtil;
 
 @Configuration
 @ComponentScan(basePackages = {"com.polaris.psi"})
 @EnableTransactionManagement(proxyTargetClass = true)
-@Import({PwfSpringConfig.class, PolarisDealersDataSource.class, PolarisDealersCommonDataSource.class, PolarisDealersExtensionDataSource.class})
-@EnableCaching
+@Import({PwfSpringConfig.class, PolarisMinneapolisDataSource.class, PolarisDealersExtensionDataSource.class, 
+	PolarisDealersDataSource.class, PolarisDealersCommonDataSource.class})
 public class AppSpringConfig {
 
 	@Bean(name = "polarisDealersAppEntityPackage")
 	public String polarisDealersAppEntityPackage() {
 		return "com.polaris.psi.repository.entity";
+	}
+
+	@Bean(name = PolarisMinneapolisDataSource.DATASOURCE_BEAN_NAME)
+	public DataSource polarisMinneapolisDataSource() {
+		return JndiUtil.lookupDataSource("as400/POLDEV");
 	}
 
 	@Bean(name = PolarisDealersExtensionDataSource.DATASOURCE_BEAN_NAME)
@@ -42,20 +45,5 @@ public class AppSpringConfig {
 	@Bean(name = PolarisDealersDataSource.DATASOURCE_BEAN_NAME)
 	public DataSource polarisDealersDataSource() {
 		return JndiUtil.lookupDataSource("sql/PolarisDealers");
-	}
-
-	@Bean
-	public ConcurrentMapCache ValidValuesCache() {
-		return new ConcurrentMapCache(Constants.VALID_VALUES_CACHE, true);
-	}
-
-	@Bean
-	public ConcurrentMapCache validValuesMapCache() {
-		return new ConcurrentMapCache(Constants.VALID_VALUES_MAP_CACHE, true);
-	}
-
-	@Bean
-	public ConcurrentMapCache allValidValuesMapCache() {
-		return new ConcurrentMapCache(Constants.VALID_VALUES_MAP_ALL_CACHE, true);
 	}
 }
