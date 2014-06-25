09aee3a3e0731a35cc90815abac16a1dcf59f4b0
Brady Erickson <brady.erickson@polaris.com>
2014-06-25 10:59:47 -0500

    Initial creation

---
+++ src/test/java/com/polaris/psi/service/mapper/PSIProfileMapperTest.java
@@ -0,0 +1,71 @@
+package com.polaris.psi.service.mapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.polaris.psi.repository.entity.PSIProfile;
+import com.polaris.psi.resource.dto.ProfileDto;
+
+public class PSIProfileMapperTest {
+	
+	private PSIProfileMapper mapper;
+	@Mock private ProfileTypeMapper mockTypeMapper;
+	@Mock private PSIProfile mockProfile;
+	@Mock private ProfileDto mockDto;
+	private String expectedName, expectedStatus, expectedType;
+	private Integer expectedProfileId;
+	private Date expectedTargetCompletion, expectedModified;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		expectedName = "UT Name";
+		expectedStatus = "UT Status";
+		expectedType = "UT Type";
+		expectedTargetCompletion = Calendar.getInstance().getTime();
+		expectedModified = Calendar.getInstance().getTime();
+		expectedProfileId = 999;
+		
+		when(mockProfile.getId()).thenReturn(expectedProfileId);
+		when(mockProfile.getLastModifiedDate()).thenReturn(expectedModified);
+		when(mockProfile.getName()).thenReturn(expectedName);
+		when(mockProfile.getStatus()).thenReturn(expectedStatus);
+		when(mockProfile.getTargetCompleteDate()).thenReturn(expectedTargetCompletion);
+		when(mockProfile.getType()).thenReturn(expectedType);
+		
+		mapper = new PSIProfileMapper();
+		mapper.typeMapper = mockTypeMapper;
+	}
+
+	@Test
+	public void testMapToDtoPSIProfile() {
+
+		ProfileDto result = mapper.mapToDto(mockProfile);
+		
+		assertEquals(expectedModified, result.getModifiedDate());
+		assertEquals(expectedName, result.getName());
+		assertEquals(expectedProfileId.intValue(), result.getProfileId());
+		assertEquals(expectedStatus, result.getStatus());
+		assertEquals(expectedTargetCompletion, result.getTargetCompletionDate());
+		
+		verify(mockProfile).getId();
+		verify(mockProfile).getLastModifiedDate();
+		verify(mockProfile).getName();
+		verify(mockProfile).getStatus();
+		verify(mockProfile).getTargetCompleteDate();
+		verify(mockProfile).getType();
+		
+		verify(mockTypeMapper).mapTypeToProfile(expectedType, result);
+	}
+
+}
