0b2497eb263e792dae692d1ce5e7dd820bd0cd45
Brady Erickson <brady.erickson@polaris.com>
2014-06-27 15:07:41 -0500

    Added string trimming

--- src/main/java/com/polaris/psi/service/mapper/DsmDealerProfileMapper.java
+++ src/main/java/com/polaris/psi/service/mapper/DsmDealerProfileMapper.java
@@ -19,13 +19,13 @@ public class DsmDealerProfileMapper {
 	public DsmDealerProfilesDto mapToDto(DealerAndDsm dealerAndDsm, PSIProfile psiProfile) {
 		DsmDealerProfilesDto ddProfile = new DsmDealerProfilesDto();
 		ddProfile.setDealerid(dealerAndDsm.getDealerId());
-		ddProfile.setDealerName(dealerAndDsm.getDealerName());
+		ddProfile.setDealerName(dealerAndDsm.getDealerName().trim());
 		ddProfile.setDsmId(dealerAndDsm.getDsmId());
-		ddProfile.setDsmName(dealerAndDsm.getDsmName());
+		ddProfile.setDsmName(dealerAndDsm.getDsmName().trim());
 		ddProfile.setModifiedDate(psiProfile.getLastModifiedDate());
 		ddProfile.setProfileId(psiProfile.getId());
 		ddProfile.setStatus(psiProfile.getStatus());
-		ddProfile.setType(dealerAndDsm.getProductLine());
+		ddProfile.setType(dealerAndDsm.getProductLine().trim());
 		
 		return ddProfile;
 	}
