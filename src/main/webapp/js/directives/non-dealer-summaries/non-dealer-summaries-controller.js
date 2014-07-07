76e04ff6a83de004871b9fcd3d68c22f943b6997
tbpitts <terry.pitts@polaris.com>
2014-07-07 13:47:35 -0500

    resumed unneeded dsm-datatable folder and contents, updates to red/green light functionality

--- src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
+++ src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
@@ -7,6 +7,14 @@
 	        .withPaginationType('full_numbers')
 	        .withDisplayLength(25)
 	        .withBootstrap();
+        
+        $scope.lightColor = function(status) {
+        	if(status === true){
+        		return 'green-light';
+        	}else{
+        		return 'red-light';
+        	}
+        };
 	       
     }
 
