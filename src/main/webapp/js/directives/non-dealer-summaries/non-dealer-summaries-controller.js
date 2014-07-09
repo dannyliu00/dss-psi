41d9541587a46f8502a33b08536ad46b7e7190f3
tbpitts <terry.pitts@polaris.com>
2014-07-09 17:27:24 -0500

    no message

--- src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
+++ src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
@@ -7,19 +7,6 @@
 	        .withPaginationType('full_numbers')
 	        .withDisplayLength(25)
 	        .withBootstrap();
-       
-//        var dealer = {dealerId: $routeParams.dealerId};
-//	    dealerResource.get(dealer).then(function(returnedDealer) {
-//	    	$scope.dealer = returnedDealer;
-//	        });
-        
-        $scope.lightColor = function(status) {
-        	if(status === true){
-        		return 'green-light';
-        	}else{
-        		return 'red-light';
-        	}
-        };
 	       
     }
 
