71bba935c60fa3641663ec6ca38d68b2c29a3eca
tbpitts <terry.pitts@polaris.com>
2014-07-09 11:48:54 -0500

    url changes to match service changes

--- src/main/webapp/js/resources/details/profile-details-resource-module.js
+++ src/main/webapp/js/resources/details/profile-details-resource-module.js
@@ -3,6 +3,5 @@
 
     angular.module('sellIn.resources.dealerProfileDetails', ['ngResource'])
         .service('dealerProfileDetailsResource', dealerProfileDetails.DealerProfileDetailsResource)
-        .constant('detailsSaveUrl', '/dss-psi/webapi/details/save')
-        .constant('detailsSubmitUrl', '/dss-psi/webapi/details/submit');
+        ;
 })();
--- src/main/webapp/js/resources/profile/dealer-profile-resource-module.js
+++ src/main/webapp/js/resources/profile/dealer-profile-resource-module.js
@@ -4,5 +4,7 @@
     angular.module('sellIn.resources.dealerProfile', ['ngResource'])
         .service('dealerProfileResource', dealerProfile.DealerProfileResource)
         .service('orderSegmentResourceMapper', dealerProfile.OrderSegmentResourceMapper)
-        .constant('profileUrl', '/dss-psi/webapi/profile/:profileId');
+        .constant('profileUrl', '/dss-psi/webapi/profile/:profileId/:dealerId')
+        .constant('profileUrl', '/dss-psi/webapi/profile/:profileId/save')
+        .constant('profileUrl', '/dss-psi/webapi/profile/:profileId/submit');
 }());
