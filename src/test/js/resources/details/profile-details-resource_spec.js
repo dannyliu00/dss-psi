50516fe1f75844fe1901da69392d50f4f99219a6
Brady Erickson <brady.erickson@polaris.com>
2014-07-09 13:30:58 -0500

    Commented out tests just before removing the file

--- src/test/js/resources/details/profile-details-resource_spec.js
+++ src/test/js/resources/details/profile-details-resource_spec.js
@@ -9,57 +9,57 @@ describe('DealerProfileDetailsResource', function() {
         });
     });
 
-    describe('save', function() {
-        it('returns a promise with a single object containing a message and array of details', inject(function(dealerProfileDetailsResource, detailsSaveUrl) {
-            var details = [{name: 'UT Detail'}, {name: 'UT Detail 2'}];
-            var expectedRequest = detailsSaveUrl;
-            var expectedMessage = 'UT response message';
-            var expectedDetails = [{id: 'UT1', name: 'UT Detail'}, {id: 'UT2', name: 'UT Detail 2'}];
-            var expectedResponse = {message: expectedMessage, objects: expectedDetails};
+//    describe('save', function() {
+//        it('returns a promise with a single object containing a message and array of details', inject(function(dealerProfileDetailsResource, detailsSaveUrl) {
+//            var details = [{name: 'UT Detail'}, {name: 'UT Detail 2'}];
+//            var expectedRequest = detailsSaveUrl;
+//            var expectedMessage = 'UT response message';
+//            var expectedDetails = [{id: 'UT1', name: 'UT Detail'}, {id: 'UT2', name: 'UT Detail 2'}];
+//            var expectedResponse = {message: expectedMessage, objects: expectedDetails};
+//
+//            httpBackend.when('POST', expectedRequest).respond(expectedResponse);
+//            httpBackend.expectPOST(expectedRequest);
+//
+//            var promise = dealerProfileDetailsResource.save(details);
+//
+//            promise.then(function(actualResponse) {
+//                expect(actualResponse.message).toEqual(expectedMessage);
+//                for(var i = 0; i < actualResponse.objects.length; i++) {
+//                    var returnedDetail = actualResponse.objects[i];
+//                    var expectedDetail = expectedDetails[i];
+//                    expect(returnedDetail.id).toEqual(expectedDetail.id);
+//                    expect(returnedDetail.name).toEqual(expectedDetail.name);
+////                }
+////            });
+//
+//            httpBackend.flush();
+//        }));
+//    });
 
-            httpBackend.when('POST', expectedRequest).respond(expectedResponse);
-            httpBackend.expectPOST(expectedRequest);
-
-            var promise = dealerProfileDetailsResource.save(details);
-
-            promise.then(function(actualResponse) {
-                expect(actualResponse.message).toEqual(expectedMessage);
-                for(var i = 0; i < actualResponse.objects.length; i++) {
-                    var returnedDetail = actualResponse.objects[i];
-                    var expectedDetail = expectedDetails[i];
-                    expect(returnedDetail.id).toEqual(expectedDetail.id);
-                    expect(returnedDetail.name).toEqual(expectedDetail.name);
-                }
-            });
-
-            httpBackend.flush();
-        }));
-    });
-
-    describe('submit', function() {
-        it('returns a promise with a single object containing a message and array of details', inject(function(dealerProfileDetailsResource, detailsSubmitUrl) {
-            var details = [{name: 'UT Detail'}, {name: 'UT Detail 2'}];
-            var expectedRequest = detailsSubmitUrl;
-            var expectedMessage = 'UT response message';
-            var expectedDetails = [{id: 'UT1', name: 'UT Detail'}, {id: 'UT2', name: 'UT Detail 2'}];
-            var expectedResponse = {message: expectedMessage, objects: expectedDetails};
-
-            httpBackend.when('POST', expectedRequest).respond(expectedResponse);
-            httpBackend.expectPOST(expectedRequest);
-
-            var promise = dealerProfileDetailsResource.submit(details);
-
-            promise.then(function(actualResponse) {
-                expect(actualResponse.message).toEqual(expectedMessage);
-                for(var i = 0; i < actualResponse.objects.length; i++) {
-                    var returnedDetail = actualResponse.objects[i];
-                    var expectedDetail = expectedDetails[i];
-                    expect(returnedDetail.id).toEqual(expectedDetail.id);
-                    expect(returnedDetail.name).toEqual(expectedDetail.name);
-                }
-            });
-
-            httpBackend.flush();
-        }));
-    });
+//    describe('submit', function() {
+//        it('returns a promise with a single object containing a message and array of details', inject(function(dealerProfileDetailsResource, detailsSubmitUrl) {
+//            var details = [{name: 'UT Detail'}, {name: 'UT Detail 2'}];
+//            var expectedRequest = detailsSubmitUrl;
+//            var expectedMessage = 'UT response message';
+//            var expectedDetails = [{id: 'UT1', name: 'UT Detail'}, {id: 'UT2', name: 'UT Detail 2'}];
+//            var expectedResponse = {message: expectedMessage, objects: expectedDetails};
+//
+//            httpBackend.when('POST', expectedRequest).respond(expectedResponse);
+//            httpBackend.expectPOST(expectedRequest);
+//
+//            var promise = dealerProfileDetailsResource.submit(details);
+//
+//            promise.then(function(actualResponse) {
+//                expect(actualResponse.message).toEqual(expectedMessage);
+//                for(var i = 0; i < actualResponse.objects.length; i++) {
+//                    var returnedDetail = actualResponse.objects[i];
+//                    var expectedDetail = expectedDetails[i];
+//                    expect(returnedDetail.id).toEqual(expectedDetail.id);
+//                    expect(returnedDetail.name).toEqual(expectedDetail.name);
+////                }
+////            });
+////
+////            httpBackend.flush();
+////        }));
+//    });
 });
