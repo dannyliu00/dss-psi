7dfb62d1bea63ebd065bfef4ac331050d799fbe0
Brady Erickson <brady.erickson@polaris.com>
2014-07-10 10:22:14 -0500

    Refactored queries to be more precise

--- src/main/java/com/polaris/pwf/dao/PSIProfileDao.java
+++ src/main/java/com/polaris/pwf/dao/PSIProfileDao.java
@@ -29,18 +29,20 @@ public class PSIProfileDao extends AbstractPolarisMinneapolisDao<PSIProfile> {
 	private static Logger LOG = Logger.getLogger(PSIProfileDao.class);
 
 	private static String QRY_DLR_CURRENT = ""
-			+ "SELECT pstatus.N2DESC, profile.N1IPID, profile.N1DESC, profile.N1TDAT, profile.N1PDLN, "
+			+ "SELECT distinct pstatus.N2DESC, profile.N1IPID, profile.N1DESC, profile.N1TDAT, profile.N1PDLN, "
 			+ "  CAST( CAST(status.N9DESC AS CHAR(50)) AS VARCHAR(50)) as N9DESC, header.N7NFLG "
 			+ "  FROM cm006f dealer INNER JOIN ot071f profile ON profile.N1PDLN = dealer.PTSFAM "
 			+ "  INNER JOIN ot072f pstatus ON pstatus.N2STID = profile.N1STID "
+			+ "  INNER JOIN ot075f osComp ON osComp.N5IPID = profile.N1IPID "
 			+ "  LEFT OUTER JOIN ot077f header ON header.N7IPID = profile.N1IPID AND header.N7DLR  = dealer.ptcust "
 			+ "  LEFT OUTER JOIN ot079f status ON header.N7STID = status.N9STID "
 			+ " WHERE dealer.ptcust = :dealerId AND dealer.PTCANDT= :canceled ";
 	private static String QUERY_BY_ID = ""
-			+ "SELECT pstatus.N2DESC, profile.N1IPID, profile.N1DESC, profile.N1TDAT, profile.N1PDLN, "
+			+ "SELECT distinct pstatus.N2DESC, profile.N1IPID, profile.N1DESC, profile.N1TDAT, profile.N1PDLN, "
 			+ "		CAST( CAST(status.N9DESC AS CHAR(50)) AS VARCHAR(50)) as N9DESC, header.N7NFLG, profile.N1LGLT, "
 			+ "		header.N7DHID, header.N7DLR, header.N7MAIL, header.N7SBDT, header.N7APDT, header.N7CRDT, header.N7CHDT "
 			+ "  FROM OT071F profile INNER JOIN OT072F pstatus ON pstatus.N2STID = profile.N1STID "
+			+ "  INNER JOIN ot075f osComp ON osComp.N5IPID = profile.N1IPID "
 			+ "  LEFT OUTER JOIN OT077F header ON header.N7IPID = profile.N1IPID "
 			+ "  LEFT OUTER JOIN OT079F status ON header.N7STID = status.N9STID "
 			+ " WHERE profile.N1IPID = :profileId";
