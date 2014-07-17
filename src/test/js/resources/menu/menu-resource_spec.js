4a57f80d737cda82d09f9463f5bac82d182bbb19
tbpitts <terry.pitts@polaris.com>
2014-07-17 16:39:39 -0500

    fixed menu resource test

--- src/test/js/resources/menu/menu-resource_spec.js
+++ src/test/js/resources/menu/menu-resource_spec.js
@@ -15,18 +15,18 @@ describe('MenuResource', function() {
     });
 
     describe('get', function() {
-        it('returns a promise containing the menulink based on the user role', inject(function(menuResource,menuUrl){
+        it('returns a promise of the menulink based on the user role', inject(function(menuResource,menuUrl){
         	
             var expectedRequest = menuUrl;
-            var expectedList = [{menuLinks: 'displayName'}];
+            var expectedMenuLinks = {category: 'data'};
             
-            httpBackend.when('Get', expectedRequest).respond(expectedList);
-            httpBackend.expectGET;
+            httpBackend.when('GET', expectedRequest).respond(expectedMenuLinks);
+            httpBackend.expectGET(expectedRequest);
 
             var promise = menuResource.get();
 
             promise.then(function(menuLinks) {
-                expect(menuLinks[0].displayName).toEqual(expectedList[0].displayName);
+                expect(menuLinks.data).toEqual(expectedMenuLinks.data);
             });
             httpBackend.flush();
         }));
