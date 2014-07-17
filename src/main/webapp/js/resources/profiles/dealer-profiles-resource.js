41567463ba54781edebc1808545f4e0deb50ed63
Brady Erickson <brady.erickson@polaris.com>
2014-07-17 12:56:03 -0500

    Fix for bug PS-160 (IE caching bug)

--- src/main/webapp/js/resources/profiles/dealer-profiles-resource.js
+++ src/main/webapp/js/resources/profiles/dealer-profiles-resource.js
@@ -2,7 +2,14 @@
     var dealerProfiles = sellInNamespace('sellIn.resources.dealerProfiles');
 
     function DealerProfilesResource($resource, profilesUrl) {
-        this.resource = $resource(profilesUrl);
+        this.resource = $resource(profilesUrl, {
+            ran: function() {
+                // parameter added to URL to work around IE caching mechanism
+                return new Date().getTime();
+            }
+        }, {
+            query: {method: 'GET', isArray: true, cache: true}
+        });
     }
 
     DealerProfilesResource.prototype.query = function(profile) {
