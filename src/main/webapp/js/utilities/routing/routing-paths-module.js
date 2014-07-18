0315ba4bfde6f78520d52da07a3d9c723e08c288
git <erickson.brady@gmail.com>
2014-07-18 15:07:18 -0500

    Added type to URL parameters

--- src/main/webapp/js/utilities/routing/routing-paths-module.js
+++ src/main/webapp/js/utilities/routing/routing-paths-module.js
@@ -4,14 +4,14 @@
         .constant('defaultPageControllerName', 'defaultPageController')
         .constant('defaultPageTemplateUrl', 'js/pages/dealer-summary/profile-summary.html')
 
-        .constant('dealerSummaryPageUrl', '/dealerSummary/:dealerId')
+        .constant('dealerSummaryPageUrl', '/dealerSummary/:dealerId/:type')
         .constant('dealerSummaryPageControllerName', 'dealerProfileSummaryCtrl')
         .constant('dealerSummaryPageTemplateUrl', 'js/pages/dealer-summary/profile-summary.html')
 
         .constant('profilePageUrl', '/dealerProfile/:dealerId/profile/:profileId/type/:type')
         .constant('profilePageControllerName', 'profileController')
         .constant('profilePageTemplateUrl', 'js/pages/profile-detail/profile-detail-template.html')
-        
+
         .constant('dsmUrl','/nonDealerSummary/:id')
         .constant('dsmControllerName','nonDealerSummaryController')
         .constant('dsmTemplateUrl','js/pages/non-dealer-summary/non-dealer-summary-page-template.html')
