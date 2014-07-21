2c43bdbbf36938356050b23fed0e21678e2d18d7
Brady Erickson <brady.erickson@polaris.com>
2014-07-21 14:26:06 -0500

    Added method to determine if rows already exists for the specified header and detail IDs

--- src/main/java/com/polaris/psi/repository/dao/PSILogDao.java
+++ src/main/java/com/polaris/psi/repository/dao/PSILogDao.java
@@ -3,6 +3,9 @@
  */
 package com.polaris.psi.repository.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
 import com.polaris.psi.repository.entity.PSILog;
@@ -17,6 +20,14 @@ public class PSILogDao extends AbstractPolarisMinneapolisDao<PSILog> {
 
 	public PSILogDao() {
 		super(PSILog.class);
+	}
+	
+	public int getLogEntryCount(int headerId, int detailId) {
+        Map<String, Object> keyMap = new HashMap<String, Object>(2);
+        keyMap.put("headerId", headerId);
+        keyMap.put("detailId", detailId);
+        
+        return selectByMap(keyMap, null).size();
 	}
 
 }
