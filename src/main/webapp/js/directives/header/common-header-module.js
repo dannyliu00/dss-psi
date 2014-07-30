a4ca044294a0ebb2aa3803936c4ba4b69e3b82a6
Brady Erickson <brady.erickson@polaris.com>
2014-07-30 13:14:34 -0500

    Reverted back to previous approach of displaying the menu

--- src/main/webapp/js/directives/header/common-header-module.js
+++ src/main/webapp/js/directives/header/common-header-module.js
@@ -4,7 +4,10 @@
 (function () {
     var commonHeader = sellInNamespace('polaris.directives.commonHeader');
 
-    angular.module('polaris.directives.commonHeader', [])
+    angular.module('polaris.directives.commonHeader', [
+        'sellIn.resources.menu',
+        'sellIn.resources.role',
+        'sellIn.resources.attribute'])
         .controller('commonHeaderController', commonHeader.CommonHeaderController)
         .directive('commonHeader', commonHeader.CommonHeaderDirective)
         .constant('appTitle', 'Inventory Profiles');
