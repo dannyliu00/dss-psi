07a7070f8be4bf6bdbcaf52451cd58ad1beb72de
Brady Erickson <brady.erickson@polaris.com>
2014-07-29 23:01:48 -0500

    Moved the main menu from the Header to its own directive

--- src/main/webapp/js/directives/header/common-header-module.js
+++ src/main/webapp/js/directives/header/common-header-module.js
@@ -4,10 +4,7 @@
 (function () {
     var commonHeader = sellInNamespace('polaris.directives.commonHeader');
 
-    angular.module('polaris.directives.commonHeader', [
-        'sellIn.resources.menu',
-        'sellIn.resources.role',
-        'sellIn.resources.attribute'])
+    angular.module('polaris.directives.commonHeader', [])
         .controller('commonHeaderController', commonHeader.CommonHeaderController)
         .directive('commonHeader', commonHeader.CommonHeaderDirective)
         .constant('appTitle', 'Inventory Profiles');
