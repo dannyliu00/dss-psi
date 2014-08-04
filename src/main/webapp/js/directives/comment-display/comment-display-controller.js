b4aa0671aeb57897c4e0fd498a8e9629de9c9cfd
Brady Erickson <brady.erickson@polaris.com>
2014-08-04 10:22:41 -0500

    Fixed indentation per JSHint rules

--- src/main/webapp/js/directives/comment-display/comment-display-controller.js
+++ src/main/webapp/js/directives/comment-display/comment-display-controller.js
@@ -1,35 +1,35 @@
 (function() {
-	var commentDisplay = sellInNamespace('sellIn.directives.commentdisplay');
+    var commentDisplay = sellInNamespace('sellIn.directives.commentdisplay');
 
     function CommentDisplayController($scope) {
-    	
-    	var showGuide = { dealer: '', 
-    					 dsm: '',
-    					 rsm: ''
-    			};
-    	
-    	function show() {
-    		if($scope.role.rsm === true || $scope.role.dsm === true) {
-    			showGuide.dealer = true;
-    			showGuide.dsm = true;
-    			showGuide.rsm = true;
-    		} else if($scope.role.dealer === true && ($scope.profile.status === 'RETURNED TO DEALER' || $scope.profile.status === 'APPROVED WITH CHANGES')) {
-    			showGuide.dealer = true;
-    			showGuide.dsm = true;
-    			showGuide.rsm = false;
-    		} else {
-    			showGuide.dealer = true;
-    			showGuide.dsm = false;
-    			showGuide.rsm = false;
-    		}
-    		return showGuide;
-    	}
-    	
-    	$scope.showComments = show();
-    	$scope.viewDealerComments = $scope.profile.orderSegments[0].dealerComments;
-		$scope.viewDsmComments = $scope.profile.orderSegments[0].dsmComments;
-		$scope.viewRsmComments = $scope.profile.orderSegments[0].adminComments; 	
+
+        var showGuide = { dealer: '',
+            dsm: '',
+            rsm: ''
+        };
+
+        function show() {
+            if($scope.role.rsm === true || $scope.role.dsm === true) {
+                showGuide.dealer = true;
+                showGuide.dsm = true;
+                showGuide.rsm = true;
+            } else if($scope.role.dealer === true && ($scope.profile.status === 'RETURNED TO DEALER' || $scope.profile.status === 'APPROVED WITH CHANGES')) {
+                showGuide.dealer = true;
+                showGuide.dsm = true;
+                showGuide.rsm = false;
+            } else {
+                showGuide.dealer = true;
+                showGuide.dsm = false;
+                showGuide.rsm = false;
+            }
+            return showGuide;
+        }
+
+        $scope.showComments = show();
+        $scope.viewDealerComments = $scope.profile.orderSegments[0].dealerComments;
+        $scope.viewDsmComments = $scope.profile.orderSegments[0].dsmComments;
+        $scope.viewRsmComments = $scope.profile.orderSegments[0].adminComments;
     }
-    
+
     commentDisplay.CommentDisplayController = CommentDisplayController;
 })();
