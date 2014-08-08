5b3538a55405a8762d55553b8c23d5f872589fe9
Brady Erickson <brady.erickson@polaris.com>
2014-08-08 11:37:50 -0500

    Implemented write Inventory Profile to Stocking Profile system functionality

--- src/main/java/com/polaris/psi/repository/dao/DealerProfileOrderDao.java
+++ src/main/java/com/polaris/psi/repository/dao/DealerProfileOrderDao.java
@@ -3,20 +3,47 @@
  */
 package com.polaris.psi.repository.dao;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Repository;
 
-import com.polaris.psi.repository.entity.PolarisOrder;
+import com.polaris.psi.repository.entity.SegmentStockingProfileOrder;
+import com.polaris.psi.util.PolarisIdentity;
+import com.polaris.psi.util.SplunkLogger;
 import com.polaris.pwf.dao.AbstractPolarisMinneapolisDao;
+import com.polaris.pwf.dao.SegmentStockingProfileDao;
 
 /**
  * @author bericks
  *
  */
 @Repository
-public class DealerProfileOrderDao extends AbstractPolarisMinneapolisDao<PolarisOrder> {
+public class DealerProfileOrderDao extends AbstractPolarisMinneapolisDao<SegmentStockingProfileOrder> {
 
+	private static final SplunkLogger LOG = new SplunkLogger(SegmentStockingProfileDao.class);
+	
 	public DealerProfileOrderDao() {
-		super(PolarisOrder.class);
+		super(SegmentStockingProfileOrder.class);
+	}
+	
+	public SegmentStockingProfileOrder retrieve(String profile, String segment, int period) {
+        Map<String, Object> keyMap = new HashMap<String, Object>(3);
+        keyMap.put("profileName", profile);
+        keyMap.put("detailToken", segment);
+        keyMap.put("profilePeriod", period);
+        
+        List<SegmentStockingProfileOrder> profiles = selectByMap(keyMap, null);
+        
+        try {
+            if(profiles.size() > 1) throw new Exception();
+        } catch (Exception e) {
+        	LOG.error(PolarisIdentity.get(), "retrieve", "We expected to get a List with a single item. The List contained " + profiles.size() + " instead.");
+        	return null;
+        }
+        
+        return profiles.get(0);
 	}
 	
 }
