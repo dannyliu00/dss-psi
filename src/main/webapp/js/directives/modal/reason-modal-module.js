087cf929ec27f985d2730bec40e9a7f25969dd37
pceder <per.cedersund@polaris.com>
2014-08-15 10:50:28 -0500

    Added remaining translation

--- src/main/webapp/js/directives/modal/reason-modal-module.js
+++ src/main/webapp/js/directives/modal/reason-modal-module.js
@@ -1,7 +1,7 @@
 (function () {
     var reasonModal = sellInNamespace('sellIn.directives.reasonmodal');
 
-    angular.module('sellIn.directives.reasonmodal', ['ngRoute', 'ui.bootstrap', 'sellIn.resources.reasoncode'])
+    angular.module('sellIn.directives.reasonmodal', ['ngRoute', 'ui.bootstrap', 'sellIn.resources.reasoncode', 'sellIn.services.translation'])
         .controller('reasonModalController', reasonModal.ReasonModalController)
     ;
 })();
