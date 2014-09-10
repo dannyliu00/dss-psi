d0f97bcaa55b0dfd2bd3e057721250ef21d1e912
tbpitts <terry.pitts@polaris.com>
2014-09-10 14:02:24 -0500

    addition of requested sorting on dsm and rsm views

--- src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
+++ src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-controller.js
@@ -1,12 +1,9 @@
 (function () {
     var nonDealerSummaries = sellInNamespace('sellIn.directives.nondealersummaries');
 
-    function NonDealerSummariesController($scope, DTOptionsBuilder) {
-        $scope.dtOptions = DTOptionsBuilder.newOptions()
-            .withPaginationType('full_numbers')
-            .withDisplayLength(25)
-            .withBootstrap();
+    function NonDealerSummariesController($scope) {
+    	//no-op
     }
-
+    
     nonDealerSummaries.NonDealerSummariesController = NonDealerSummariesController;
 })();
--- src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-directive.js
+++ src/main/webapp/js/directives/non-dealer-summaries/non-dealer-summaries-directive.js
@@ -1,14 +1,12 @@
 (function () {
     var nonDealerSummaries = sellInNamespace('sellIn.directives.nondealersummaries');
 
-    function NonDealerSummariesDirective($http, $compile, $templateCache) {
+    function NonDealerSummariesDirective($http, $compile, $templateCache, DTOptionsBuilder) {
 
         return {
             restrict: 'E',
             controller: nonDealerSummaries.NonDealerSummariesController,
             link: function (scope, element, attrs) {
-                var basePath = 'js/directives/non-dealer-summaries/';
-                var pathSuffix = '-summary-datatables-template.html';
 
                 scope.$watch(attrs.profiles, function (profiles) {
                     if (profiles != null) {
@@ -16,13 +14,28 @@
                         scope.$watch(attrs.role, function (role) {
                             if (role != null) {
                                 var templateUrl = '';
-
+                                var basePath = 'js/directives/non-dealer-summaries/';
+                                var pathSuffix = '-summary-datatables-template.html';
+                                
+                                //DT Options set in directive due to controller instantiating prior to directive.  Creates and 
+                                //prevents load.
+                                
                                 if (role.rsm === true || role.admin === true) {
                                     templateUrl = basePath + "rsm" + pathSuffix;
                                     loadTemplate(templateUrl);
+                                    scope.dtOptions = DTOptionsBuilder.newOptions()
+	                		            .withPaginationType('full_numbers')
+	                		            .withDisplayLength(25)
+	                		            .withBootstrap()
+	                		            .withOption('order',[[1,"asc"],[3,"asc"]]);
                                 } else if (role.dsm === true) {
                                     templateUrl = basePath + "dsm" + pathSuffix;
                                     loadTemplate(templateUrl);
+                                    scope.dtOptions = DTOptionsBuilder.newOptions()
+	                		            .withPaginationType('full_numbers')
+	                		            .withDisplayLength(25)
+	                		            .withBootstrap()
+	                		            .withOption('order',[2,"asc"]);
                                 }
                             }
                         });
